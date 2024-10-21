a323aaa996721d6e1245f82bc46e81cdf0f8f468
Ati <nagyonkellvalamihez@gmail.com>
2024-10-21 16:54:22 +0200

    adding the formatted recipe the difficulty

--- src/commands.ts
+++ src/commands.ts
@@ -5,7 +5,7 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
   const formatted = recipes
-    .map((recipe) => `- [${recipe.id}] ${recipe.name}`)
+    .map((recipe) => `- [${recipe.id}] ${recipe.name} ${recipe.difficulty}`)
     .join('\n');
   console.log('Your recipes:');
   console.log(formatted);
