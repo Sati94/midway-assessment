dcf9a9944b6d69efd808587eab7f48f2b5d16834
Ati <nagyonkellvalamihez@gmail.com>
2024-10-21 16:47:59 +0200

    creating a new function name: details

--- src/commands.ts
+++ src/commands.ts
@@ -10,3 +10,18 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
   console.log('Your recipes:');
   console.log(formatted);
 }
+
+
+export async function details(store: Store<RecipeType[]>, args: string[]) {
+  const id = args[0];
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+  const foundRecipe = recipes.find(recipe => recipe.id === Number(id));
+  if (!foundRecipe) {
+    console.error(`Recipe with ID ${id} not found.`);
+  } else {
+    console.log(`ID: ${foundRecipe.id}`);
+    console.log(`Name: ${foundRecipe.name}`);
+  }
+
+}
